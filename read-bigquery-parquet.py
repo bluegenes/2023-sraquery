3977e68616250d34b321a264aa63ee6f3b5fc63a
N. Tessa Pierce-Ward <ntpierce@gmail.com>
2023-10-12 09:36:02 -0700

    write list of accs

--- read-bigquery-parquet.py
+++ read-bigquery-parquet.py
@@ -10,9 +10,11 @@ human_df = df[df['organism'].str.contains('human', case=False)]
 # count organism column values
 human_df['organism'].value_counts()
 
-# look at platform counts 
+# look at platform counts
 human_df['platform'].value_counts()
 
+# write all accs to csv
+df['acc'].to_csv('20230802-mgx.acc.csv', index=False)
 
 # write the human metadata to a csv file
 human_df.to_csv("20230802-human-mgx.metadata.csv.gz", index=False)
@@ -22,3 +24,6 @@ human_df[['acc', 'organism']].to_csv("20230802-human-mgx.acc-organism.csv.gz", i
 
 # write acc organism file for just illumina platform
 human_df[human_df['platform'] == 'ILLUMINA'][['acc', 'organism']].to_csv("20230802-human-mgx.acc-organism.illumina.csv.gz", index=False)
+
+# find 10 smalles mbases rows (smallest number of megabases sequenced)
+df.nsmallest(10, 'mbases')
